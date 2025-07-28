e1af89b826cbcac2509b312c55f442e63e86a8cf
Ndubuisi Chibuogwu <nduonochie@gmail.COM>
2025-07-28 13:51:27 -0500

    Update pdm_dashboard.py

--- pages/pdm_dashboard.py
+++ pages/pdm_dashboard.py
@@ -68,7 +68,7 @@ st.markdown("""
 
     .big-font {
         font-size: 18px !important;
-        color: #2e7d32;
+        color: #0B3D0B;
         text-shadow: 1px 1px 1px rgba(0,0,0,0.1);
     }
 
